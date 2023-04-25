3e64bb8849b06689c02d7bebf81866b229b4bca2
chubedan <chubedan@gmail.com>
2023-04-25 21:30:30 +0700

    update

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,18 +3,19 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const authHeader = req.header("Authorization");
   const token = authHeader && authHeader.split(" ")[1];
-
-  if (!token) return res.json({ status: 0, message: "not sign-in yet" });
-
+  if (!token || token === null || token === "null") {
+    return res.json({ status: 0, message: "chưa đăng nhập" });
+  }
   try {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     console.log(decoded._id, "decoded._id");
     req.userId = decoded._id;
+    req.mess = "token còn hiệu lực";
     next();
   } catch (error) {
     return res.json({
       status: 0,
-      message: "u need to login again because of expired token",
+      message: "token đã hết hạn, cần đăng nhập lại",
     });
   }
 };
--- modules/users/user.Controller.js
+++ modules/users/user.Controller.js
@@ -9,9 +9,32 @@ const registerUser = async (req, res, next) => {
   const { username, email, password } = req.body;
   try {
     if (username && email && password) {
-      const userExists = await User.findOne({ username: username });
-      if (userExists) {
-        res.json({ status: 0, message: "User already exists" });
+      // const checkDuplicate = await User.findOne({
+      //   $or: [
+      //     { username: "chubedan12" },
+      //     { email: "chubej121233123ohnwick@gmail.com" },
+      //   ],
+      // });
+      // if (usernameExists) {
+      //   res.json({
+      //     status: 0,
+      //     message:
+      //       "Tên đăng nhập hoặc email này đã được đăng ký, vui lòng thử lại",
+      //   });
+      // }
+      const usernameExists = await User.findOne({ username: username });
+      if (usernameExists) {
+        return res.json({
+          status: 0,
+          message: "Tên đăng nhập này đã được đăng ký, vui lòng thử lại",
+        });
+      }
+      const userEmailExists = await User.findOne({ email: email });
+      if (userEmailExists) {
+        return res.json({
+          status: 0,
+          message: "email này đã được sử dụng, vui lòng thử lại",
+        });
       }
       const newUser = await User.create({
         username,
@@ -19,10 +42,13 @@ const registerUser = async (req, res, next) => {
         password,
       });
       if (newUser) {
-        return res.status(200).json("Create user successfully");
+        return res.status(200).json("Tạo tài khoản thành công");
       }
     } else {
-      res.json({ status: 0, message: "Please fill all the fields" });
+      res.json({
+        status: 0,
+        message: "Vui lòng điền hết các trường thông tin",
+      });
     }
   } catch (error) {
     res.json({ status: 0, message: error.message });
@@ -82,7 +108,9 @@ const posts = async (req, res, next) => {
   try {
     const user = await User.findOne({ _id: { $eq: req.userId } });
     console.log(user, "user");
-    return res.status(200).json({ status: 1, message: "success", user: user });
+    return res
+      .status(200)
+      .json({ status: 1, message: "thành công", user: user, mess: req.mess });
   } catch (error) {
     return res.json({ status: 0, message: error.message });
   }
