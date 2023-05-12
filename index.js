e1b25eb7c74fac90ef92378922ca74efe3d95974
chubedan <chubedan@gmail.com>
2023-05-12 17:56:57 +0700

    234

--- index.js
+++ index.js
@@ -15,16 +15,6 @@ const app = express();
 app.use(express.json());
 
 // Enable CORS for all requests
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
 
 app.use(cors());
 
